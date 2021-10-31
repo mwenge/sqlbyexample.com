6172c829d1ea8249c27e78d114f8b9915bb14aba
Robert Hogan <robhogan@gmail.com>
2021-10-31 11:42:53 +0000

    Update examples

--- examples.js
+++ examples.js
@@ -1,10 +1,32 @@
 const examples = [
   {
-    sql: "SELECT * FROM Customers",
+    sql: `
+SELECT * 
+FROM Artist
+`,
     title: "Select All",
   },
+
   {
-    sql: "SELECT * FROM Artist",
-    title: "Select All",
+    sql: `
+SELECT * 
+FROM Artist
+LIMIT 10
+`,
+    title: `
+Select a maximum of 10 records
+`,
+  },
+
+  {
+    sql: `
+SELECT i.billingcountry, sum(total) as 'TotalSales'
+FROM invoice AS i
+GROUP BY billingcountry
+ORDER BY totalsales DESC
+`,
+    title: `
+Aggregate using Sum
+`,
   },
 ]
